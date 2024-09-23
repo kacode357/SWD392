31059d876afefe23a040441fb4ad2d545f336331
kaka <113968332+Ka213s@users.noreply.github.com>
2024-09-23 16:54:01 +0700

    Update

--- src/pages/User/VerifyEmail.tsx
+++ src/pages/User/VerifyEmail.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { verifyEmailApi } from '../../util/api';
 import { Spin, Result } from 'antd';
