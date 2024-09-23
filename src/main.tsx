ec76a288bb8cd273566e7b8a47b0492815201606
kaka <kacode357@gmail.com>
2024-09-23 17:06:14 +0700

    Update  fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import Router from '../src/router/Router';
+import Router from './router/RouterWeb';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
