8160582c7ba393933d0141c6b83872fd36347b8d
kaka <kacode357@gmail.com>
2024-09-23 17:03:45 +0700

    Update

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import Router from './router/Router'; // assuming your router file is handling your routes
+import Router from '../src/router/Router';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
