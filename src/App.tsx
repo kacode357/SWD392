98c8d959010ef363fb67822dc028b10de116582c
Ka Ka Lưu <kacode357@gmail.com>
2024-10-14 19:08:22 +0700

    Change url api

--- src/App.tsx
+++ src/App.tsx
@@ -54,6 +54,7 @@ const App: React.FC = () => {
         }
       } catch (error) {
         console.error('Failed to fetch account', error);
+        localStorage.clear();
       } finally {
         setAppLoading(false);
       }
