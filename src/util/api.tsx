cadb915567f6a705fd6ee255bf3326d2a42f89f3
Ka Ka Lưu <kacode357@gmail.com>
2024-10-26 07:17:08 +0700

    create page payment

--- src/util/api.tsx
+++ src/util/api.tsx
@@ -358,8 +358,15 @@ const getPaymentStatusApi = async (params: { vnp_Amount: string; vnp_BankCode: s
   const response = await defaultAxiosInstance.get(URL_API);
   return response.data;
 };
+const createPaymentApi = async (params: { vnp_Amount: string; vnp_BankCode: string; vnp_BankTranNo: string; vnp_CardType: string; vnp_OrderInfo: string; vnp_PayDate: string; vnp_ResponseCode: string; vnp_TmnCode: string; vnp_TransactionNo: string; vnp_TransactionStatus: string; vnp_TxnRef: string; vnp_SecureHash: string; }) => {
+  const queryString = new URLSearchParams(params).toString();
+  const URL_API = `/api/payment/createpayment?${queryString}`;
+  const response = await defaultAxiosInstance.get(URL_API);
+  return response.data;
+};
 
 export {
+  createPaymentApi,
   getPaymentStatusApi,
   getUrlPaymentApi,
   deleteItemInCartApi,
